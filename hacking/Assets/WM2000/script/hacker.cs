11f1e973937bdc2c03dddf6e83eb8059bd04d858
Sireen Yasser Albalkhi <albalkhisyreen@gmail.com>
2020-12-23 18:59:41 +0300

    hacker

---
+++ hacking/Assets/WM2000/script/hacker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        MainMenu("welcome to this hacking game!!");
+    }
+
+    void MainMenu(string greeting)
+    {
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("press 1 to hack the bank");
+        Terminal.WriteLine("press 2 to hack bus station");
+        Terminal.WriteLine("press 3 to hack the airport");
+
+
+    }
+}
