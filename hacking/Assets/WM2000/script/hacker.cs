e586a2bf65ec4ee870bcf152b351b5306b0d0461
Sireen Yasser Albalkhi <albalkhisyreen@gmail.com>
2020-12-24 15:16:47 +0300

    hackers

--- hacking/Assets/WM2000/script/hacker.cs
+++ hacking/Assets/WM2000/script/hacker.cs
@@ -2,23 +2,110 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hacker : MonoBehaviour
+public class اش : MonoBehaviour
 {
+
+    int level;
+    string currentscreen = "mainmenu";
+
     // Start is called before the first frame update
     void Start()
     {
+
+        showmainmenu("welcome to hackers world !!!");
+
         
-
-        MainMenu("welcome to this hacking game!!");
     }
 
-    void MainMenu(string greeting)
+    // Update is called once per frame
+    void Update()
     {
-        Terminal.WriteLine(greeting);
+        
+    }
+
+    void showmainmenu(string greetings)
+    {
+
+        Terminal.WriteLine(greetings);
         Terminal.WriteLine("press 1 to hack the bank");
-        Terminal.WriteLine("press 2 to hack bus station");
-        Terminal.WriteLine("press 3 to hack the airport");
+        Terminal.WriteLine("press 2 to hack anonymous hackers organisation" );
 
 
     }
+
+    void onuserinput (string input)
+    {
+        if(currentscreen == "mainmenu")
+        {
+            setlevel(input);
+
+        }
+        else if(currentscreen == "password")
+        {
+
+
+        }
+
+        void setlevel(string selectedlevel)
+        {
+            if(selectedlevel == "1")
+            {
+                level = 1;
+                print(level);
+               randompass();
+            }
+
+            else if(selectedlevel == "2")
+            {
+                level = 2;
+                print(level);
+                randompass();
+            }
+            else
+            {
+                Terminal.WriteLine("wrong input. please enter a valid input.");
+            }
+
+
+        }
+
+        void startgame()
+        {
+            Terminal.WriteLine("You have chosen level " + level);
+            currentscreen = "password";
+        }
+
+        void randompass()
+        {
+            startgame();
+            switch (level)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
